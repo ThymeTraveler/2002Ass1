a4d3b3a9a39d3792c566866713621cb6597fbde1
rayhaan2120 <rayhaan2120@gmail.com>
2021-08-21 19:17:46 +0200

    Added some code for sequential median filter

--- src/medianFilter.java
+++ src/medianFilter.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 public class medianFilter{
     
     public static void main(String[] args){
-        System.out.println("Joe papa");
+
+        String filename = "sampleInput1000.txt";
+        String line ="";
+        try{
+            File myFile = new File(filename);
+            Scanner lines = new Scanner(myFile);
+            ArrayList<Integer> items = new ArrayList<Integer>();
+
+            while (lines.hasNextLine()){
+                line = lines.nextLine();
+                items.add(Integer.parseInt(line));
+            }
+
+        } catch(FileNotFoundException e){
+            System.out.println("Oopsie diasy we could not find "+filename);
+            System.exit(0);
+
+        }   
+        
     }
 
 }
