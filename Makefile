dd420b3b5e3b37483591f2e48ae6dfecb491963c
rayhaan2120 <rayhaan2120@gmail.com>
2021-08-22 11:09:17 +0200

    A lot has changed

--- Makefile
+++ Makefile
@@ -11,23 +11,18 @@ DOC=doc
 
 $(BINDIR)/%.class:$(SRCDIR)/%.java
 	$(JAVAC) -d $(BINDIR)/ -cp $(BINDIR) $<
-CLASSES= BinaryTreeNode.class BTQueueNode.class BTQueue.class BinaryTree.class BinarySearchTree.class AVLTree.class AccessAVLApp.class
+CLASSES= medianFilter.class
 CLASS_FILES=$(CLASSES:%.class=$(BINDIR)/%.class)
-SOURCES= AccessArrayApp.java 
+SOURCES= medianFilter.java 
 SOURCE_FILES= $(SOURCES:%.java=$(SRCDIR)/%.java)
 
 default: $(CLASS_FILES)
 
 clean: 	
 	rm $(BINDIR)/*.class
-runarray: $(CLASS_FILES)
-	java -cp $(BINDIR) AccessArrayApp
-
-runbst: $(CLASS_FILES)
-	java -cp $(BINDIR) AccessBSTApp
-
-runavl:  $(CLASS_FILES)
-	java -cp $(BINDIR) AccessAVLApp
+runseq: $(CLASS_FILES)
+	java -cp $(BINDIR) medianFilter
+	@echo  $("sampleInput100.txt 3 output.txt")
 
 doc: $(SOURCE_FILES)
 	javadoc -version -author -d $(DOC) $(SOURCE_FILES)
--- src/medianFilter.java
+++ src/medianFilter.java
@@ -2,15 +2,22 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.lang.Math;
+import java.util.Collections;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class medianFilter{
     
     public static void main(String[] args){
 
-        String filename = "sampleInput100.txt";
+        String filename = args[0];
+        String outputName = args[2];
+        int filterSize = Integer.parseInt(args[1]);
+
         String line ="";
         String item="";
-        ArrayList<Float> items = new ArrayList<Float>();
+        ArrayList<Double> items = new ArrayList<Double>();
         try{
             File myFile = new File(filename);
             Scanner lines = new Scanner(myFile);
@@ -22,7 +29,7 @@ public class medianFilter{
                 itemScanner.next();
                 item=itemScanner.next();
                 item=item.replaceAll(",",".");
-                items.add(Float.parseFloat(item));
+                items.add(Double.parseDouble(item));
             }
 
         } catch(FileNotFoundException e){
@@ -30,8 +37,66 @@ public class medianFilter{
             System.exit(0);
 
         }
+
+        ArrayList<Double> filteredList = (applyFilter(items, filterSize));
+        printToFile(filteredList, outputName);
         
+    }
+
+    public static ArrayList<Double> applyFilter(ArrayList<Double> input1,int filterSize){
+        ArrayList<Double> input = new ArrayList<Double>(input1);
+        ArrayList<Double> result = new ArrayList<Double>();
+        int border= (int) Math.ceil(filterSize/2);
+        ArrayList<Double> localItems = new ArrayList<Double>();
+        int size = (input.size());
         
+        for (int i= 0; i<border;i++){
+            result.add(input.get(i));
+            
+            
+        }
+        
+        for (int i= border; i<size-border;i++){
+            for (int x =i-border;x<i+border+1;x++){
+                localItems.add(input.get(x));
+                
+                
+            }
+            Collections.sort(localItems);
+            result.add(localItems.get(border));
+            localItems.clear();
+            
+        }
+
+        for (int i= size-border; i<size;i++){
+            result.add(input.get(i));
+        }
+        
+
+
+        return result;
+    }
+
+    public static void printToFile(ArrayList<Double> input, String outputName){
+
+       try{
+        FileWriter output = new FileWriter(outputName);
+       
+            output.write(Integer.toString(input.size())+"\n");
+            for(int i=0;i<input.size();i++){
+                output.write(String.format(i + " "+ "%.5f",input.get(i))+"\n");  
+            }
+            output.close();
+
+       }catch(IOException e){
+           System.out.println("Damn bro, looks like I can't do that one");
+       }
+
+
+
+
+        
+
     }
 
 }
